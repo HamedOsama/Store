a8236a40dfde657dbeabb6ac68e68e2270792c92
Hamed Osama <hamedosama19@gmail.com>
2022-05-11 06:27:24 +0200

    init by typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 const port = 3000;
 
 const app = express();
+// middleware to parse incoming request
+app.use(express.json());
+// Middleware
+app.use(morgan('common'));
+// http security middleware
+app.use(helmet());
+// limit requests to increase security
+app.use(
+    rateLimit({
+        windowMs: 60 * 60 * 1000, // 15 minutes
+        max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        message: 'TOO MANY REQUESTS!!',
+    })
+);
+
+// get request
 app.get('/', (req, res) => {
     res.send('hello world!');
 });
+// post request
+app.post('/', (req, res) => {
+    res.send('hello world from post!');
+    console.log(req.body);
+});
+// listening on port
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
