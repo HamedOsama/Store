a934f56c94ddd52989fa2bb63efe8bc7d9390e87
Hamed Osama <hamedosama19@gmail.com>
2022-05-11 04:37:12 +0200

    init by typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(1);
+import express from 'express';
+const port = 3000;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('hello world!');
+});
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+
+export default app;
